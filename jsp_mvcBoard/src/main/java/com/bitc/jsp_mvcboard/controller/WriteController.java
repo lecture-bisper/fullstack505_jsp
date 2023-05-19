7a075ad8f4a6e4a2dff8823b2b3a092b38400d60
lecture-bisper <bisper@outlook.kr>
2023-05-19 17:00:09 +0900

    1. 게시글 등록 시 첨부 파일 기능 추가.

--- jsp_mvcBoard/src/main/java/com/bitc/jsp_mvcboard/controller/WriteController.java
+++ jsp_mvcBoard/src/main/java/com/bitc/jsp_mvcboard/controller/WriteController.java
@@ -2,13 +2,19 @@ package com.bitc.jsp_mvcboard.controller;
 
 import com.bitc.jsp_mvcboard.model.MVCBoardDAO;
 import com.bitc.jsp_mvcboard.model.MVCBoardDTO;
+import com.bitc.jsp_mvcboard.util.FileUtils;
+import com.bitc.jsp_mvcboard.util.JSFunc;
+import com.oreilly.servlet.MultipartRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @WebServlet("/mvcBoard/Write.do")
 public class WriteController extends HttpServlet {
@@ -24,18 +30,47 @@ public class WriteController extends HttpServlet {
     req.setCharacterEncoding("UTF-8");
 
 //    1. 클라이언트에서 전송된 내용 가져오기
-    String title = req.getParameter("title");
-    String writer = req.getParameter("writer");
-    String pass = req.getParameter("pass");
-    String contents = req.getParameter("contents");
+//    String title = req.getParameter("title");
+//    String writer = req.getParameter("writer");
+//    String pass = req.getParameter("pass");
+//    String contents = req.getParameter("contents");
+
+//    현재 파일 정보에 대한 내용이 없음
+//    파일 정보를 가져오는 처리가 필요함
+//    String saveDir = req.getServletContext().getRealPath("/upload");
+//    서버에 저장할 위치
+    String saveDir = "C:\\upload";
+//    업로드 파일 최대 용량 제한
+    int maxSize = 10 * 1024 * 1024;
+    // MultipartRequest 로 데이터 가져오기
+    MultipartRequest mr = FileUtils.uploadFile(req, saveDir, maxSize);
+
+    if (mr == null) {
+      JSFunc.alertLocation(resp, "첨부 파일의 크기가 큽니다.", "/mvcBoard/write.do");
+      return;
+    }
 
     MVCBoardDTO board = new MVCBoardDTO();
 
-    board.setPostTitle(title);
-    board.setPostWriter(writer);
-    board.setPostPass(pass);
-    board.setPostContent(contents);
-//    파일은 나중에 구현
+    board.setPostTitle(mr.getParameter("title"));
+    board.setPostWriter(mr.getParameter("writer"));
+    board.setPostPass(mr.getParameter("pass"));
+    board.setPostContent(mr.getParameter("content"));
+
+    String fileName = mr.getFilesystemName("file");
+
+    if (fileName != null) {
+      String now = new SimpleDateFormat("yyyyMMdd_HmsS").format(new Date());
+      String ext = fileName.substring(fileName.lastIndexOf("."));
+      String newFileName = now + ext;
+
+      File oldFile = new File(saveDir + File.separator + fileName);
+      File newFile = new File(saveDir + File.separator + newFileName);
+      oldFile.renameTo(newFile);
+
+      board.setPostOfile(fileName);
+      board.setPostSfile(newFileName);
+    }
 
 //    2. 데이터 베이스 연결
     MVCBoardDAO dao = new MVCBoardDAO();
